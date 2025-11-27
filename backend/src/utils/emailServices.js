e7b335e516d44cad78996dd65d6ff14fb96a0b6f
Himanshu Yadav <ny6873064@gmail.com>
2025-11-27 16:47:00 +0530

    .

--- backend/src/utils/emailServices.js
+++ backend/src/utils/emailServices.js
@@ -3,18 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();  
 
 const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    secure: false,                             // true for 465, false for other ports
-    auth: {
-        user: process.env.SMTP_USER,          
-        pass: process.env.SMTP_PASS           //less secured pass
-    },
-    tls: {
-        ciphers: 'SSLv3',
-        rejectUnauthorized: false
-    } 
+  host: process.env.SMTP_HOST,
+  port: 587,
+  secure: false, 
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
 });
+
 
 export const sendEmail = async (to, subject, html, text) => {
   const info = await transporter.sendMail({
